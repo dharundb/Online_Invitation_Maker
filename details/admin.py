8c9b110682f8cdd6c5b97e9c3b23714e066616f4
Dharun Bharathi S <63294700+dharundb@users.noreply.github.com>
2021-11-12 00:52:31 +0530

    Add files via upload

---
+++ details/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(wed_credential)
+admin.site.register(gath_credential)
+admin.site.register(bday_credential)
+admin.site.register(Media)
